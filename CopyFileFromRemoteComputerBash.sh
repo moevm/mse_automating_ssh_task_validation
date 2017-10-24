bc4fb9007f2366972e6f95bb6c7710ee70712a93
kruglikalexey <kruglikalexey@ya.ru>
2017-10-24 14:48:24 +0300

    Update CopyFileFromRemoteComputerBash.sh

--- CopyFileFromRemoteComputerBash.sh
+++ CopyFileFromRemoteComputerBash.sh
@@ -1,6 +1,7 @@
 # This script provides the ability to configure a virtual machine in order
 # to prepare it for this challenge. It runs for every virtual machine created
 # prior to assigning it to a user. The execution time is limited to 5 minutes.
+#Необходимо добавить запрет на использование комманды cp,перемещение, echo и touch
 sudo sh -c 'echo HISTFILE=/home/box/.bash_history >> /.box/bashrc.d/box/rules' 
 sudo sh -c $'echo "export PROMPT_COMMAND=\'history -a;history -r\'" > /.box/bashrc.d/box/rules' 
 PROMPT_COMMAND="history -a"
@@ -12,8 +13,8 @@ sudo chsh -s /bin/bash erdos
 sudo chmod 777 /home
 sudo -S -u erdos mkdir /home/erdos
 sudo -S -u erdos chmod 700 /home/erdos
-sudo -S -u erdos echo "Star wars, Harry Potter, Lord of the rings - cool." > /home/erdos/block_and_anton.txt
-sudo -S -u erdos echo "And for uncheating - holly random:" > /home/erdos/block_and_anton.txt
+sudo -S -u erdos echo "Star wars, Harry Potter, Lord of the rings and Erdos - cool." > /home/erdos/block_and_anton.txt
+sudo -S -u erdos echo "And for uncheating - holly random:" >> /home/erdos/block_and_anton.txt
 sudo -S -u erdos echo $(( ( RANDOM % 5304 )  + 1 )) >> /home/erdos/block_and_anton.txt
 sudo chmod 755 /home
 
